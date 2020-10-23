a92b5f7ca2502da640265b0ab11ce31997eba0d7
Mofe Salami <mofe.salami@yapily.com>
2020-10-23 12:50:28 +0100

    Added support for handling multiple query parameters with the same name

--- display-parameters-server/src/main/java/com/divitngoc/controller/MainController.java
+++ display-parameters-server/src/main/java/com/divitngoc/controller/MainController.java
@@ -1,9 +1,12 @@
 package com.divitngoc.controller;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,9 +14,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MainController {
 
 	@GetMapping("/")
-	public String main(@RequestParam final Map<String, String> requestParams, final ModelMap model) {
-		model.addAttribute("parameters", requestParams);
+	public String main(@RequestParam final MultiValueMap<String, String> duplicateRequestParams, final ModelMap model) {
+		System.out.println("Test: " + duplicateRequestParams);
+		final Map<String, String> uniqueRequestParams = new HashMap<>();
+
+		Iterator<String> it = duplicateRequestParams.keySet().iterator();
+		while(it.hasNext()) {
+			String theKey = (String) it.next();
+			if (duplicateRequestParams.get(theKey).size() > 1) {
+				for (int index = 0; index < duplicateRequestParams.get(theKey).size(); index++) {
+					String duplicatedValue = duplicateRequestParams.get(theKey).get(index);
+					uniqueRequestParams.put(theKey + " ["+(index+1)+"]", duplicatedValue);
+				}
+			} else {
+				uniqueRequestParams.put(theKey, duplicateRequestParams.getFirst(theKey));
+			}
+		}
+
+		model.addAttribute("parameters", uniqueRequestParams);
 		return "display-parameters"; // view
 	}
-
 }
